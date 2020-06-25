c6d171812c5779ba52ead45ee1cc0461339e4b39
Lukasz Szczotka <lukaszszczo@gmail.com>
2020-06-25 03:05:36 +0200

    11.2

---
+++ kodilla-soap/src/main/java/com/kodilla/soap/library/BookRepository.java
@@ -0,0 +1,52 @@
+package com.kodilla.soap.library;
+
+import com.kodilla.courses.soap.Book;
+import com.kodilla.courses.soap.Course;
+import com.kodilla.courses.soap.Genre;
+import com.kodilla.courses.soap.Technology;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Date;
+
+@Component
+public class BookRepository {
+    private static final Map<String, Book> books = new HashMap<>();
+
+    @PostConstruct
+    public void initData() {
+        Book javaFirst = new Book();
+        javaFirst.setId("1/3");
+        javaFirst.setGenre(Genre.TECH);
+        javaFirst.setTitle("Pierwsza ksiązka");
+        javaFirst.setTitle("Pierwszy autor");
+
+
+        books.put(javaFirst.getId(), javaFirst);
+
+        Book javaSecond = new Book();
+        javaSecond.setId("2/3");
+        javaSecond.setGenre(Genre.CRIME);
+        javaSecond.setTitle("Druga ksiązka");
+        javaSecond.setTitle("Drugi autor");
+
+
+        books.put(javaSecond.getId(), javaSecond);
+
+        Book javaThird = new Book();
+        javaThird.setId("3/3");
+        javaThird.setGenre(Genre.FANTASY);
+        javaThird.setTitle("Trzecia ksiązka");
+        javaThird.setTitle("Trzeci autor");
+
+
+        books.put(javaThird.getId(), javaThird);
+    }
+
+    public Book findBook(String name) {
+        Assert.notNull(name, "You have to specify the books's id");
+        return books.get(name);
+    }
+}
