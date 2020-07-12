9f270c1d6ede93c95d9800a6061756cb8080c8ac
Lukasz Szczotka <lukaszszczo@gmail.com>
2020-07-12 23:20:55 +0200

    cucumber

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/visual/OcularExample.java
@@ -0,0 +1,28 @@
+package com.kodilla.selenium.visual;
+
+import com.testautomationguru.ocular.Ocular;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.nio.file.Paths;
+
+public class OcularExample {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "c:/selenium-drivers/chrome/chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+
+        Ocular.config()
+                .resultPath(Paths.get("c:/ocular/result"))
+                .snapshotPath(Paths.get("c:/ocular/snapshot"))
+                .globalSimilarity(95)
+                .saveSnapshot(true);
+
+        WorldTimePage page = new WorldTimePage(webDriver);
+        page.open();
+        page.compare();
+        page.close();
+
+    }
+
+
+}
