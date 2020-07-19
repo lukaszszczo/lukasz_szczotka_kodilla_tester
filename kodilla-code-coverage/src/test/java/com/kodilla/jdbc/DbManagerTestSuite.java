03904eb41f0f8562fcae4f5ea958412cf6d769b7
Lukasz Szczotka <lukaszszczo@gmail.com>
2020-07-19 22:50:17 +0200

    15.2

---
+++ kodilla-code-coverage/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -0,0 +1,110 @@
+package com.kodilla.jdbc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbManagerTestSuite {
+    @Test
+    public void testConnect() throws SQLException {
+        //Given
+        //When
+        DbManager dbManager = DbManager.getInstance();
+        //Then
+        Assert.assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    public void testSelectUsers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String countQuery = "SELECT COUNT(*) FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(countQuery);
+        int count = 0;
+        while (rs.next()) {
+            count = rs.getInt("COUNT(*)");
+        }
+        String sql = "INSERT INTO USERS(FIRSTNAME, LASTNAME) VALUES ('Zara', 'Ali')";
+        statement.executeUpdate(sql);
+        sql = "INSERT INTO USERS(FIRSTNAME, LASTNAME) VALUES ('Otman', 'Use')";
+        statement.executeUpdate(sql);
+        sql = "INSERT INTO USERS(FIRSTNAME, LASTNAME) VALUES ('Mark', 'Boq')";
+        statement.executeUpdate(sql);
+        sql = "INSERT INTO USERS(FIRSTNAME, LASTNAME) VALUES ('Uli', 'Wimer')";
+        statement.executeUpdate(sql);
+        sql = "INSERT INTO USERS(FIRSTNAME, LASTNAME) VALUES ('Oli', 'Kosiw')";
+        statement.executeUpdate(sql);
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        statement = dbManager.getConnection().createStatement();
+        rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+
+
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        int expected = count + 5;
+        Assert.assertEquals(expected, counter);
+
+        rs.close();
+        statement.close();
+    }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String countQuery = "SELECT COUNT(*) FROM USERS JOIN POSTS ON USERS.ID = POSTS.USER_ID GROUP BY POSTS.USER_ID HAVING COUNT(*) >= 2";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(countQuery);
+        int count = 0;
+        while (rs.next()) {
+            count = rs.getInt("COUNT(*)");
+        }
+        String sql = "INSERT INTO POSTS (USER_ID, BODY) VALUES (2,'This is my first post too!')";
+        statement.executeUpdate(sql);
+        sql = "INSERT INTO POSTS (USER_ID, BODY) VALUES (2,'This is my first post too!')";
+        statement.executeUpdate(sql);
+        sql = "INSERT INTO POSTS (USER_ID, BODY) VALUES (3,'Testowa wiadomosc')";
+        statement.executeUpdate(sql);
+        sql = "INSERT INTO POSTS (USER_ID, BODY) VALUES (3,'Testowa wiadomosc')";
+        statement.executeUpdate(sql);
+
+        //When
+        String sqlQuery = "SELECT FIRSTNAME, LASTNAME, BODY FROM USERS INNER JOIN POSTS ON USERS.ID = POSTS.USER_ID HAVING COUNT(*) >= 2";
+        statement = dbManager.getConnection().createStatement();
+        rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            String firstname = rs.getString("USERS.FIRSTNAME");
+            String lastname = rs.getString("USERS.LASTNAME");
+            String body = rs.getString("POSTS.BODY");
+
+            System.out.println(firstname + " " + lastname + " " + body);
+            counter++;
+
+            counter++;
+        }
+        int expected = count + 2;
+        Assert.assertEquals(expected, counter);
+
+        rs.close();
+        statement.close();
+
+
+    }
+}
